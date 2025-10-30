76053ce3c1ccdbde87645fac8e9f6e95b8a86e38
Ivan Lepakhin <ivan.lepakhin@firstlinesoftware.com>
2025-10-31 00:21:46 +0100

    update

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,6 +1,10 @@
-require("mason-lspconfig").setup({
-  automatic_enable = {
-    "copilot",
-    "powershell_es",
+return {
+  "mason-org/mason-lspconfig.nvim",
+  opts = {
+    ensure_installed = { "copilot", "powershell_es" },
   },
-})
+  dependencies = {
+    { "mason-org/mason.nvim", opts = {} },
+    "neovim/nvim-lspconfig",
+  },
+}
