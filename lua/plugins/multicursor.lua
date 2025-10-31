15fdafe903b2752018e663ef7bfd741a6ca27996
Ivan Lepakhin <ivan.lepakhin@firstlinesoftware.com>
2025-10-31 13:45:00 +0100

    update

--- lua/plugins/multicursor.lua
+++ lua/plugins/multicursor.lua
@@ -1,18 +1,16 @@
 -- lazy.nvim:
 return {
-  "smoka7/multicursors.nvim",
+  "mg979/vim-visual-multi",
   event = "VeryLazy",
-  dependencies = {
-    "nvimtools/hydra.nvim",
-  },
-  opts = {},
-  cmd = { "MCstart", "MCvisual", "MCclear", "MCpattern", "MCvisualPattern", "MCunderCursor" },
-  keys = {
-    {
-      mode = { "v", "n" },
-      "<Leader>m",
-      "<cmd>MCstart<cr>",
-      desc = "Create a selection for selected text or word under the cursor",
-    },
-  },
+  init = function()
+    vim.g.VM_default_mappings = false
+    vim.g.VM_maps = {
+      ["Find Under"] = "<C-d>", -- Ctrl+d для выделения слова
+      ["Find Subword Under"] = "<C-d>",
+      ["Add Cursor Down"] = "<C-Down>", -- или <C-j>
+      ["Add Cursor Up"] = "<C-Up>", -- или <C-k>
+      ["Goto Next"] = "<C-n>",
+      ["Goto Prev"] = "<C-p>",
+    }
+  end,
 }
