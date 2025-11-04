153ac0bc4656733c02baa5808f61a91c37ab8b29
Ivan Lepakhin <ivan.lepakhin@firstlinesoftware.com>
2025-11-04 10:21:45 +0100

    update

--- lua/plugins/multicursor.lua
+++ lua/plugins/multicursor.lua
@@ -5,8 +5,8 @@ return {
   init = function()
     vim.g.VM_default_mappings = false
     vim.g.VM_maps = {
-      ["Find Under"] = "<C-d>", -- Ctrl+d для выделения слова
-      ["Find Subword Under"] = "<C-d>",
+      ["Find Under"] = "<C-z>", -- Ctrl+d для выделения слова
+      ["Find Subword Under"] = "<C-z>",
       ["Add Cursor Down"] = "<C-j>", -- или <C-j>
       ["Add Cursor Up"] = "<C-k>", -- или <C-k>
       ["Goto Next"] = "<C-n>",
