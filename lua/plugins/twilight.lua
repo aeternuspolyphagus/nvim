d6af0033a7a48e45d4df01b8af62e76d7940fab5
Ivan Lepakhin <ivan.lepakhin@firstlinesoftware.com>
2025-11-13 13:27:24 +0100

    updaye

--- lua/plugins/twilight.lua
+++ lua/plugins/twilight.lua
@@ -1,9 +1,28 @@
--- Lua
 return {
   "folke/twilight.nvim",
   opts = {
-    -- your configuration comes here
-    -- or leave it empty to use the default settings
-    -- refer to the configuration section below
+    -- Базовые настройки (по умолчанию; кастомизируйте при необходимости)
+    dimming = {
+      alpha = 0.25, -- Уровень затемнения
+      color = { "Normal", "#ffffff" }, -- Цвет затемнения
+      term_bg = "#000000", -- Фон терминала
+      inactive = false, -- Затемнять неактивные окна (если не тот же буфер)
+    },
+    context = 10, -- Количество строк контекста вокруг текущей
+    treesitter = true, -- Использовать Tree-sitter
+    expand = {
+      "function", -- Типы узлов для расширения
+      "method",
+      "table",
+      "if_statement",
+    },
+    exclude = {}, -- Исключаемые типы файлов
   },
+  config = function(_, opts)
+    require("twilight").setup(opts)
+    local map = vim.keymap.set
+    map("n", "<leader>ctt", "<cmd>Twilight<cr>", { desc = "Toggle Twilight" })
+    map("n", "<leader>cte", "<cmd>TwilightEnable<cr>", { desc = "Enable Twilight" })
+    map("n", "<leader>ctd", "<cmd>TwilightDisable<cr>", { desc = "Disable Twilight" })
+  end,
 }
