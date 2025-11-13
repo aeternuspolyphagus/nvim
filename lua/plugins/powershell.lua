fa008b4e6db6e9df3baef3fe4169694934fdba92
Ivan Lepakhin <ivan.lepakhin@firstlinesoftware.com>
2025-11-13 12:22:22 +0100

    update

--- lua/plugins/powershell.lua
+++ lua/plugins/powershell.lua
@@ -1,6 +1,6 @@
 return {
   "TheLeoP/powershell.nvim",
-  dependencies = { "mason-org/mason.nvim" }, -- Запуск после Mason
+  dependencies = { "mason-org/mason.nvim" },
   ---@type powershell.user_config
   opts = {
     bundle_path = vim.fn.stdpath("data") .. "/mason/packages/powershell-editor-services",
