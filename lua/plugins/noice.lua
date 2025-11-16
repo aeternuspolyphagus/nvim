e13ce33ba0020975e3fdbcdca4c190e59f742397
Ivan Lepakhin <ivan.lepakhin@firstlinesoftware.com>
2025-11-16 15:51:27 +0100

    update

---
+++ lua/plugins/noice.lua
@@ -0,0 +1,10 @@
+return {
+  "folke/noice.nvim",
+  opts = {
+    presets = {
+      bottom_search = false, -- Переносим поиск в центр
+      command_palette = true,
+      long_message_to_split = true,
+    },
+  },
+}
