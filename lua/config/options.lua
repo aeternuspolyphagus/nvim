357e34ac76b347a8b43376199c88109124176e96
Ivan Lepakhin <ivan.lepakhin@firstlinesoftware.com>
2025-11-18 11:07:45 +0100

    update

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,8 +2,8 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.opt.wrap = true
-vim.opt.linebreak = true
-vim.opt.breakindent = true
+-- vim.opt.linebreak = true
+-- vim.opt.breakindent = true
 if vim.fn.has("win32") == 1 then
   vim.opt.shell = "pwsh.exe"
   vim.opt.shellcmdflag = "-NoProfile -NoLogo -ExecutionPolicy RemoteSigned -Command"
