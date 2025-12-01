26ee4fea68bcc4bf0466e2c81021504c2b80de7c
Ivan Lepakhin <ivan.lepakhin@firstlinesoftware.com>
2025-12-01 13:55:15 +0100

    update

---
+++ lua/plugins/buffer/highlite.lua
@@ -0,0 +1,41 @@
+-- ~/.config/nvim/lua/plugins/hipatterns.lua
+return {
+  {
+    "nvim-mini/mini.hipatterns",
+    version = false, -- Используйте последнюю версию
+    lazy = false, -- Загружайте сразу для ранней инициализации
+    config = function()
+      local hipatterns = require("mini.hipatterns")
+      hipatterns.setup({
+        highlighters = {
+          critical = {
+            pattern = { "FIXME", "BUG", "ERROR", "FAIL" },
+            group = "MiniHipatternsCritical",
+            extmark_opts = { priority = 200 },
+          },
+          warning = {
+            pattern = { "HACK", "WARNING", "PERF", "OPTIMIZE" },
+            group = "MiniHipatternsWarning",
+            extmark_opts = { priority = 200 },
+          },
+          note = {
+            pattern = { "NOTE", "INFO", "HINT", "IDEA", "TODO" },
+            group = "MiniHipatternsNote",
+            extmark_opts = { priority = 200 },
+          },
+          important = {
+            pattern = { "CRITICAL", "URGENT", "DEADLINE", "REVIEW" },
+            group = "MiniHipatternsImportant",
+            extmark_opts = { priority = 200 },
+          },
+          hex_color = hipatterns.gen_highlighter.hex_color({ priority = 200 }),
+        },
+      })
+      local hi = vim.api.nvim_set_hl
+      hi(0, "MiniHipatternsCritical", { fg = "#f4a261", bg = "#9a3412", bold = true })
+      hi(0, "MiniHipatternsWarning", { fg = "#000000", bg = "#fbbf24", bold = true }) -- Тёмный текст на жёлтом
+      hi(0, "MiniHipatternsNote", { fg = "#ffffff", bg = "#34d399", bold = true }) -- Белый текст на зелёном
+      hi(0, "MiniHipatternsImportant", { fg = "#000000", bg = "#a78bfa", bold = true, underline = true }) -- Тёмный текст на фиолетовом
+    end,
+  },
+}
--- lua/plugins/disable.lua
+++ lua/plugins/disable.lua
@@ -1,3 +1,6 @@
 return {
   { "nvim-mini/mini.pairs", enabled = false },
+  { "nvim-mini/mini.ai", enabled = false },
+  { "folke/todo-comments.nvim", enabled = false },
+  { "catppuccin/nvim", enabled = false },
 }
