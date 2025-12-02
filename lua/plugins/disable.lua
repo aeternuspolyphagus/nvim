63ff4511ed2ceb00b3a7eae4bab9d4ea43df1952
Ivan Lepakhin <ivan.lepakhin@firstlinesoftware.com>
2025-12-02 13:42:11 +0100

    update

--- lua/plugins/disable.lua
+++ lua/plugins/disable.lua
@@ -3,4 +3,7 @@ return {
   { "nvim-mini/mini.ai", enabled = false },
   { "folke/todo-comments.nvim", enabled = false },
   { "catppuccin/nvim", enabled = false },
+  { "folke/ts-comments.nvim", enabled = false },
+  { "folke/lazydev.nvim", enabled = false },
+  { "MagicDuck/grug-far.nvim", enabled = false },
 }
