542f5a7d17c50a746de0dc8c53a7e17263612da8
Ivan Lepakhin <ivan.lepakhin@firstlinesoftware.com>
2026-02-24 13:36:45 +0100

    update

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,17 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+vim.keymap.set("n", "<leader>bsp", function()
+  vim.cmd([[
+    %s/\v([(\[{])\zs\S/ \0/g
+    %s/\v\S\ze[)\]}]/\0 /g
+  ]])
+end, { desc = "Add spaces inside brackets" })
+
+vim.keymap.set("v", "<leader>sp", function()
+  vim.cmd([[
+    '<,'>s/\v([(\[{])\zs\S/ \0/g
+    '<,'>s/\v\S\ze[)\]}]/\0 /g
+  ]])
+end, { desc = "Add spaces inside brackets (selection)" })
