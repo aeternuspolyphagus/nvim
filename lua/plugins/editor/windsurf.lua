8f7681a4a8f7c72427bd36de0df6bbaac99d2c7e
Ivan Lepakhin <ivan.lepakhin@firstlinesoftware.com>
2026-03-09 14:50:34 +0100

    update

--- lua/plugins/editor/windsurf.lua
+++ lua/plugins/editor/windsurf.lua
@@ -4,17 +4,10 @@ return {
     event = "InsertEnter",
     config = function()
       require("codeium").setup({
-        enable_cmp_source = false, -- отключаем cmp integration
+        enable_cmp_source = false,
         enable_chat = false,
-        virtual_text = { enabled = true },
-
-        key_bindings = {
-          accept = "<Tab>", -- принять текущее предложение
-          accept_word = false,
-          accept_line = false,
-          next = "<A-n>", -- Alt+n = следующий вариант
-          prev = "<A-N>", -- Alt+Shift+n = предыдущий вариант
-          clear = "<S-c>", -- Shift+c = очистка ghost text
+        virtual_text = {
+          enabled = true,
         },
       })
     end,
